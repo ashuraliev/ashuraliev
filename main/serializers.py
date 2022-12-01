3cab0924c43ee686183b1bfe5f21c25169d80ec3
Adilet <adiletashuraliev1@gmail.com>
2022-12-01 10:40:16 +0600

    create comment model

---
+++ main/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Post
+
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    # def to_representation(self, instance):
+    #     rep = super().to_representation(instance)
+    #     rep["author"] = instance.author.username
+    #     return rep
